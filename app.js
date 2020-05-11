113822cd53521d85a0966005f2dc0c9201410bd5
Ljunghster <mpljungh@gmail.com>
2020-05-11 10:53:10 -0700

    node using variables

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-let x = 5+5
-let y = 5-5
-let z = 5*5
-let w = 5/5
+// let x = 5+5
+// let y = 5-5
+// let z = 5*5
+// let w = 5/5
 
-console.log(x)
-console.log(y)
-console.log(z)
-console.log(w)
+// console.log(x)
+// console.log(y)
+// console.log(z)
+// console.log(w)
+
+const adder = (a, b) => {
+    return a + b
+}
+
+const subtractor = (a, b) => {
+    return a - b
+}
+
+const multiplier = (a, b) => {
+    return a * b
+}
+
+const divider = (a, b) => {
+    return a / b
+}
+console.log(adder(5, 7))
+console.log(subtractor(5, 7))
+console.log(multiplier(5, 7))
+console.log(divider(5, 7))
